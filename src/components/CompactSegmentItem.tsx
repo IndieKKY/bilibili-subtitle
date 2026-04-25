842828d3c92d7e77c83935d1bb34862e8bf71b05
GaoXiang233 <1679562189@qq.com>
2026-04-25 13:02:08 +0800

    feat: 首次提交我的代码

--- src/components/CompactSegmentItem.tsx
+++ src/components/CompactSegmentItem.tsx
@@ -1,6 +1,7 @@
 import React, {useMemo} from 'react'
 import {useAppSelector} from '../hooks/redux'
-import {getDisplay, getTransText} from '../utils/bizUtil'
+import {getDisplay, getFontSizeClass, getTransText} from '../utils/bizUtil'
+import {TRANS_DISPLAY_DUAL} from '../consts/const'
 import classNames from 'classnames'
 
 const CompactSegmentItem = (props: {
@@ -15,14 +16,26 @@ const CompactSegmentItem = (props: {
   const transResult = useAppSelector(state => state.env.transResults[idx])
   const envData = useAppSelector(state => state.env.envData)
   const fontSize = useAppSelector(state => state.env.envData.fontSize)
+  const customFontSize = useAppSelector(state => state.env.envData.customFontSize)
   const autoTranslate = useAppSelector(state => state.env.autoTranslate)
   const transText = useMemo(() => getTransText(transResult, envData.hideOnDisableAutoTranslate, autoTranslate), [autoTranslate, envData.hideOnDisableAutoTranslate, transResult])
   const display = useMemo(() => getDisplay(envData.transDisplay, item.content, transText), [envData.transDisplay, item.content, transText])
 
-  return <div className={classNames('inline', fontSize === 'large'?'text-sm':'text-xs')}>
+  const fontSizeClass = getFontSizeClass(fontSize)
+  const isDualMode = envData.transDisplay === TRANS_DISPLAY_DUAL
+
+  const fontSizeStyle = useMemo(() => {
+    if (fontSize === 'custom' && customFontSize) {
+      return { fontSize: `${customFontSize}px` }
+    }
+    return {}
+  }, [fontSize, customFontSize])
+
+  return <div className={classNames('inline', fontSizeClass)} style={fontSizeStyle}>
     <span className={'pl-1 pr-0.5 py-0.5 cursor-pointer rounded-sm hover:bg-base-200'} onClick={moveCallback} onDoubleClick={move2Callback}>
       <text className={classNames('font-medium', isIn ? 'text-primary underline' : '')}>{display.main}</text>
-      {display.sub && <text className='desc'>({display.sub})</text>}</span>
+      {display.sub && <text className='desc'>{isDualMode ? ` | ${display.sub}` : `(${display.sub})`}</text>}
+    </span>
     <span className='text-base-content/75'>{!last && ','}</span>
   </div>
 }
--- src/redux/currentTimeReducer.ts
+++ src/redux/currentTimeReducer.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface CurrentTimeState {
   currentTime?: number
+  playbackRate?: number
 }
 
 const initialState: CurrentTimeState = {
-  currentTime: undefined
+  currentTime: undefined,
+  playbackRate: 1,
 }
 
 export const slice = createSlice({
@@ -14,10 +16,13 @@ export const slice = createSlice({
   reducers: {
     setCurrentTime: (state, action: PayloadAction<number | undefined>) => {
       state.currentTime = action.payload
-    }
+    },
+    setPlaybackRate: (state, action: PayloadAction<number | undefined>) => {
+      state.playbackRate = action.payload ?? 1
+    },
   }
 })
 
-export const { setCurrentTime } = slice.actions
+export const { setCurrentTime, setPlaybackRate } = slice.actions
 
 export default slice.reducer
